802075a85b95b253e3b19e95969c5a0232c3bb18
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-01-02 12:12:36 +0200

    Added empty components files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import StatusLabel, { Status } from './components/status-label';
 import { headers } from 'next/headers';
 
 export default function Home() {
-  console.log(headers());
+  // console.log(headers());
   return (
     <main>
       <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
